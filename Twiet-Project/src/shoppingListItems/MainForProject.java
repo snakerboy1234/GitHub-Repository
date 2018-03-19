a3a6fb0cfc0a9a4c99ac57fa4a9485ab08af9adc
snakerboy1234 <osipovmoj@gmail.com>
2018-03-19 07:23:36 -0700

    Update
    sd

--- Twiet-Project/src/shoppingListItems/MainForProject.java
+++ Twiet-Project/src/shoppingListItems/MainForProject.java
@@ -1,11 +1,14 @@
 package shoppingListItems;
 
+import Item.itemSetUp;
+
 public class MainForProject {
 
 	
 	public static long startTime = 0;//change this
 	
 	public static void main (String[] arg) {
+		ItemSetUp();
 		StartUpProcedure.startUp();
 		startTime = System.currentTimeMillis();
 		GUIRunner.createAndShowGUI();
